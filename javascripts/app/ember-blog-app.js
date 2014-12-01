22f63670b0abf85e8a85b6bf699115333d2ce82b
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-12-01 12:00:45 -0700

    Step 3. Added 'BlogPost' Model and Ember Data dependency

--- javascripts/app/ember-blog-app.js
+++ javascripts/app/ember-blog-app.js
@@ -12,3 +12,9 @@ EmberBlog = Ember.Application.create({
     console.log('Ember is ready!');
   }
 });
+
+// A simple client-side model for a blog post
+EmberBlog.BlogPost = DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('string')
+});
